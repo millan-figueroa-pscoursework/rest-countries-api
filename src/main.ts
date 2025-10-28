9d1173ccbb8979efdc5b429fa82302862f3f387e
millan-figueroa <millan.fig@gmail.com>
2025-10-28 15:30:31 -0700

    Creates interfaces for country.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './style.css'
 
-// const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>('#app')!
 
-// app.innerHTML = `
-//   <h1>REST Countries API</h1>
-//   <p>This is being rendered by main.ts</p>
-// `
+app.innerHTML = `
+  <h1>REST Countries API</h1>
+  <p>This is being rendered by main.ts</p>
+`
 
