8c7a3e4af7901ab2f5fa90e215ebac413d69b320
millan-figueroa <millan.fig@gmail.com>
2025-10-30 01:14:07 -0700

    Creates renderGrid function that renders the data (flags) fetched from the API

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { fetchCountries } from "./api/countries";
 import { APIError, CountryLoadError } from "./utils/errorHandler";
+import type { Country } from "./models/interfaces";
 
 const grid = document.querySelector<HTMLDivElement>("#countries-grid")!;
 
 fetchCountries()
     .then(data => {
-        console.log(data)
+        renderGrid(data)
     })
     .catch(err => {
         if (err instanceof APIError) {
@@ -20,3 +21,42 @@ fetchCountries()
 
         console.error(err);
     });
+
+
+// ***FUNCTION TO RENDER FLAGS FETCHED FROM API
+
+function renderGrid(list: Country[]) {
+    grid.innerHTML = list
+        .map((country) => {
+            const flag = country.flags?.png || country.flags?.svg || "";
+            const name = country.name.common;
+            const population = country.population.toLocaleString();
+            const region = country.region;
+            const capital = country.capital?.[0] ?? "N/A";
+
+            return `
+      <div class="bg-white dark:bg-slate-800 rounded-md shadow-md hover:shadow-lg transition 
+                  overflow-hidden cursor-pointer">
+        
+        <img src="${flag}"
+             alt="${name} flag"
+             class="w-full h-40 object-cover" />
+
+        <div class="px-6 py-6">
+          <h2 class="font-bold text-lg mb-4">${name}</h2>
+
+          <p class="text-sm mb-1">
+            <span class="font-semibold">Population: </span>${population}
+          </p>
+          <p class="text-sm mb-1">
+            <span class="font-semibold">Region: </span>${region}
+          </p>
+          <p class="text-sm">
+            <span class="font-semibold">Capital: </span>${capital}
+          </p>
+        </div>
+      </div>
+    `;
+        })
+        .join("");
+}
