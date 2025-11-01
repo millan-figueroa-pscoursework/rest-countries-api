634c6e05b14e6630754d895b8f7586e9047ed205
millan-figueroa <millan.fig@gmail.com>
2025-11-01 00:23:26 -0700

    Imports custom font, updates tailwind and styles config, updates spacing and sizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['"Nunito Sans"', "sans-serif"],
+      },
+    },
+  },
 };
